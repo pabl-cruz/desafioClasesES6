fa5a16970848c30517b01125c9264e87f3c6c641
Pablo Cruz Salinas <pablo.cruz2497@gmail.com>
2024-08-07 11:38:24 -0400

    hecho documento html y funciones para acceder mediante interfaz gráfica

--- dist/main.js
+++ dist/main.js
@@ -5,9 +5,25 @@ var _client = require("./client.mjs");
 //importar modulos
 
 //crear instancia de impuesto y cliente
-var impuestos1 = new _impuestos.Impuestos(200000, 32000);
-var cliente1 = new _client.Cliente("Juan Perez", impuestos1);
+var impuestosPrueba = new _impuestos.Impuestos(200000, 32000);
+var clientePrueba = new _client.Cliente("Juan Perez", impuestosPrueba);
 
 //imprimir instancia de cliente e impuestos
-console.log(cliente1);
-console.log(cliente1.calcularImpuesto());
+console.log(clientePrueba);
+console.log(clientePrueba.calcularImpuesto());
+
+//funcion para calcular impuestos desde interfaz en documento html
+document.getElementById("enviar").addEventListener("click", function (e) {
+  e.preventDefault();
+  //valores inputs
+  var nombreCliente = document.getElementById("nombre").value;
+  var montoBruto = document.getElementById("montoBruto").value;
+  var deduccion = document.getElementById("deduccion").value;
+
+  //instancias objeto
+  var impuestos = new _impuestos.Impuestos(Number(montoBruto), Number(deduccion));
+  var cliente = new _client.Cliente(nombreCliente, impuestos);
+
+  //mostrar datos en docmento
+  document.getElementById('datos').innerHTML = "\n        <p><strong>Nombre del cliente:</strong> ".concat(cliente._nombre, "</p>\n        <p><strong>Monto bruto:<strong> ").concat(impuestos._montoBrutoAnual, "</p>\n        <p><strong>Monto a deducir:</strong> ").concat(impuestos._deducciones, "</p>\n        <p><strong>Impuesto calculado:</strong> ").concat(cliente.calcularImpuesto(), "</p>\n    ");
+});
--- src/main.mjs
+++ src/main.mjs
@@ -3,9 +3,31 @@ import { Impuestos } from "./impuestos.mjs";
 import { Cliente } from "./client.mjs";
 
 //crear instancia de impuesto y cliente
-const impuestos1 = new Impuestos(200000, 32000);
-const cliente1 = new Cliente("Juan Perez", impuestos1);
+const impuestosPrueba = new Impuestos(200000, 32000);
+const clientePrueba = new Cliente("Juan Perez", impuestosPrueba);
 
 //imprimir instancia de cliente e impuestos
-console.log(cliente1);
-console.log(cliente1.calcularImpuesto());
+console.log(clientePrueba);
+console.log(clientePrueba.calcularImpuesto());
+
+
+//funcion para calcular impuestos desde interfaz en documento html
+document.getElementById("enviar").addEventListener("click", function(e){
+    e.preventDefault();
+    //valores inputs
+    const nombreCliente =  document.getElementById("nombre").value;
+    const montoBruto = document.getElementById("montoBruto").value;
+    const deduccion = document.getElementById("deduccion").value;
+
+    //instancias objeto
+    const impuestos = new Impuestos(Number(montoBruto),Number(deduccion));
+    const cliente = new Cliente(nombreCliente, impuestos);
+
+    //mostrar datos en docmento
+    document.getElementById('datos').innerHTML = `
+        <p><strong>Nombre del cliente:</strong> ${cliente._nombre}</p>
+        <p><strong>Monto bruto:<strong> ${impuestos._montoBrutoAnual}</p>
+        <p><strong>Monto a deducir:</strong> ${impuestos._deducciones}</p>
+        <p><strong>Impuesto calculado:</strong> ${cliente.calcularImpuesto()}</p>
+    `
+})
